4fe517a0b173e3e19850f27c821a7a09f8ec27a8
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-11-03 22:24:43 +0530

    Initialisation

--- src/Init2symblink.java
+++ src/Init2symblink.java
@@ -1,7 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 public class Init2symblink {
+    public static void main(String[] args) throws IOException {
+        String filePath = "D:\1,1M.txt";
 
+        File file = new File(filePath);
+        System.out.println(file.length());
+        long start = System.currentTimeMillis();
+        Files.lines(Path.get(filePath)).filter(str -> str.contains("gmail")).forEach(System.out::println);
 
+    }
 
-
+    }
 
 }
