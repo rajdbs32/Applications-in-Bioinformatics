a69706b044dd629b1b61ffd7d0d7b30dcd1d1e4a
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-11-03 22:27:08 +0530

    Initialisation

--- src/Init2symlink.java
+++ src/Init2symlink.java
@@ -3,14 +3,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-public class Init2symblink {
+
+public class Init2symlink {
     public static void main(String[] args) throws IOException {
         String filePath = "D:\1,1M.txt";
 
         File file = new File(filePath);
         System.out.println(file.length());
         long start = System.currentTimeMillis();
-        Files.lines(Path.get(filePath)).filter(str -> str.contains("gmail")).forEach(System.out::println);
+        Files.lines(Paths.get(filePath)).filter(str -> str.contains("gmail")).forEach(System.out::println);
 
     }
 
