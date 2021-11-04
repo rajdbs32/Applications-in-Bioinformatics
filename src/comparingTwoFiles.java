01e29de6b4cf1743f2bac6502a2fe9e81a69139c
RAJDEEP RAHA <52234042+rajdbs32@users.noreply.github.com>
2021-11-04 19:29:51 +0530

    comparing two files fasta specifically errors solved. program works but throws ignore able EXception.

--- src/comparingTwoFiles.java
+++ src/comparingTwoFiles.java
@@ -1,18 +1,19 @@
 import java.io.*;
 import java.util.*;
-
+import java.util.Scanner;
 public class comparingTwoFiles {
     public static void main(String[] args ) throws FileNotFoundException {
         String first = "", second = "";
-        String firstName = "", secondName ="";
+        String firstName = "",
+                secondName ="";
 
         Scanner input =  new Scanner(System.in);
-        System.out.print("Enter a first file name: ");
+        System.out.print("");
         firstName = input.nextLine();
-        System.out.print("Enter a second file name: ");
+        System.out.print("");
         secondName = input.nextLine();
-        Scanner input1 = new Scanner(new File(firstName));//read first file
-        Scanner input2 = new Scanner(new File(secondName));//read second file
+        Scanner input1 = new Scanner(new File(firstName));//read first file //i have to input first file location.
+        Scanner input2 = new Scanner(new File(secondName));//read second file // i have to input second file location.
 
         while(input1.hasNextLine() && input2.hasNextLine()){
             first = input1.nextLine();
